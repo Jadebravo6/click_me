3b60a0c659fe0f14ca348ee9c618c1a85283c996
Jadebravo6 <141968705+Jadebravo6@users.noreply.github.com>
2023-12-28 16:49:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,32 @@ app.use('/', express.static(path.join(__dirname + '/public')));
 // Global variables
 const port = 3000;
 
+// Fonction pour mettre à jour le chronomètre
+function updateChronometer() {
+    seconds++;
+
+    if (seconds >= 10) {
+        clearInterval(intervalId); // Arrêter le chronomètre
+        io.emit('end chrono', seconds);
+    }
+
+    io.emit('update chrono', seconds);
+}
+
+// Initialisation des variables
+let seconds = 0;
+let intervalId;
+
 io.on('connection', (socket) => {
+
+    socket.on('start game', () => {
+        // Démarrer le chronomètre
+        intervalId = setInterval(updateChronometer, 1000); // Mettre à jour toutes les 1 seconde
+
+        io.emit('game started');
+        // io.removeListener('start game')
+    });
+
     socket.on('new player', (uname)=>{
         let data = require(__dirname + '/data/data.json');
         
@@ -25,7 +50,7 @@ io.on('connection', (socket) => {
         fs.writeFileSync(__dirname + '/data/data.json', JSON.stringify(data, null, 2));
 
         data = data.sort((a, b) => b.score - a.score);
-        socket.emit('new player', data);
+        io.emit('user connected', data);
     })
 
     socket.on('existing player', (uname)=>{
@@ -33,8 +58,12 @@ io.on('connection', (socket) => {
 
         const playerData = data.filter((player) => player.uname == uname)[0];
 
-        data = data.sort((a, b) => b.score - a.score);
-        socket.emit('user connected', data);
+        if(playerData) {
+            data = data.sort((a, b) => b.score - a.score);
+            socket.emit('user connected', data);
+        } else {
+            socket.emit('user not connected');
+        }
     })
 
     socket.on('player clic', (uname) => {
@@ -42,22 +71,21 @@ io.on('connection', (socket) => {
         
         const playerData = data.filter((player) => player.uname == uname)[0];
         playerData.score++;
-        // data.push(playerData);
         fs.writeFileSync(__dirname + '/data/data.json', JSON.stringify(data, null, 2));
 
         data = data.sort((a, b) => b.score - a.score);
         io.emit('show current score', data);
     })
 
-    socket.on('end', (player)=>{
+    socket.on('end', ()=>{
         let data = require(__dirname + '/data/data.json');
 
-        socket.emit('resultats', data);
+        io.emit('resultats', data);
 
+        seconds = 0;
         for(player of data) {
-            data.score = 0;
+            player.score = 0;
         }
-
         fs.writeFileSync(__dirname + '/data/data.json', JSON.stringify(data, null, 2));
     });
 });
